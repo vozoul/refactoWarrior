ad5267b5b27bcde654898b0b00f51908b697e3a5
Jean-Charles Durand <jc.buchu@gmail.com>
2019-05-12 22:13:50 +0200

    Initial commit

---
+++ src/warriors/contracts/Map.java
@@ -0,0 +1,17 @@
+package warriors.contracts;
+
+/**
+ * This interface contains all data needed by the client about the game map
+ */
+public interface Map {
+
+	/**
+	 * @return The name of the map
+	 */
+	String getName();
+	
+	/**
+	 * @return the number of case
+	 */
+	int getNumberOfCase();
+}
